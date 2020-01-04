5bb57e2b09072e5defc3c7f23a85b8c955747272
7melo7 <nobugsquirtle@outlook.com>
2020-01-04 18:51:42 +0800

    commit

---
+++ anchor_pair_ks_calc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#########################################################
+# Function : find anchor-pair genes between 2 genomes   # 
+#            and calculate the KaKs value.              #
+# Version  : 1.0                                        #
+# Date     : 2020-01-04                                 #
+# Author   : Wenlei Guo                                 #
+# Contact  : Nobugsquirtle@outlook.com                  #
+# Address  : ZAFU                                       #
+#########################################################
+
+# Usage
+help_msg () {
+    echo "Usage: $0 < usage information>"
+}
